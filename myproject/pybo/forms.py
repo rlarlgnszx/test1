4f476b481c675139772d6ee9fccb6e7b40fde294
rlarlgnszx <rlarlgnszx@naver.com>
2021-02-22 03:55:22 -0800

    rlarlgnszx

--- myproject/pybo/forms.py
+++ myproject/pybo/forms.py
@@ -7,3 +7,7 @@ class QuestionForm(FlaskForm):
     subject = StringField('제목', validators=[DataRequired()])
     content = TextAreaField('내용', validators=[DataRequired()])
 # --------------------------------------------------------------------------- #
+# --------------------------------- [edit] ---------------------------------- #
+class AnswerForm(FlaskForm):
+    content = TextAreaField('내용', validators=[DataRequired('내용은 필수입력 항목입니다.')])
+# --------------------------------------------------------------------------- #
