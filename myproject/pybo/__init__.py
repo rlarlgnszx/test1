b1166e4abc516d7172e0974b08855ed8ebe610d1
rlarlgnszx <40743105+rlarlgnszx@users.noreply.github.com>
2021-02-24 01:12:04 +0900

    soongsil.making

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 # --------------------------------- [edit] ---------------------------------- #
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
+from flaskext.markdown import Markdown
 import config
 
 db = SQLAlchemy()
@@ -13,10 +13,10 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
     #ORM
-    
+
     db.init_app(app)
     migrate.init_app(app,db)
-
+    Markdown(app, extensions=['nl2br', 'fenced_code'])
     from . import models
 
     #blueprint
